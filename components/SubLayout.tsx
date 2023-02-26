7360e83e58d9901891ea95e4fc1f7b0792edb207
Bevan Hunt <bevan@bevanhunt.com>
2023-02-26 13:15:50 -0800

    add lang meta

--- components/SubLayout.tsx
+++ components/SubLayout.tsx
@@ -16,6 +16,7 @@ function SubLayout({ children }: ILayout) {
         <link rel="shortcut icon" href="/favicon.png" />
         <meta name="description" content="Tip your favorite Twitch streamers!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="content-language" content="en" />
       </Head>
       <main id="app" className="d-flex flex-column h-100" data-testid="layout">
         <NavBar />
