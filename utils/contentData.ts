b1f6c540809b9a1586b3d72b791ed08e090300de
Bevan Hunt <bevan@bevanhunt.com>
2023-02-26 20:01:47 -0800

    update main page

--- utils/contentData.ts
+++ utils/contentData.ts
@@ -2,22 +2,22 @@ const contentData = [
   {
     title: 'Solana Tips',
     description:
-      'CircleSub uses Solana - the fastest blockchain network that instantly settles.',
+      'CircleSub uses Solana - the fastest blockchain network with the lowest fees that instantly settles.',
   },
   {
     title: 'No Chargebacks',
     description:
-      'Crypto including Solana doesn\'t allow chargebacks protecting the influencer from costs and abuse.',
+      'Solana doesn\'t allow chargebacks protecting the influencer from costs and abuse.',
   },
   {
     title: 'Tip Alerts',
     description:
-      'Tippers get an automated personal shoutout in chat with the SOL amount of the tip. Influencers get an OBS tip alert for their stream.',
+      'Tippers get an automated personal shoutout in chat with the Solana (SOL) amount of the tip. Influencers get an OBS tip alert for their stream.',
   },
   {
     title: 'Embedded Twitch',
     description:
-      'Influencers can link directly to their tip page showing their stream/chat - useful for Linktr.ee.',
+      'Influencers can link directly to their tip page showing their stream and chat - useful for Linktr.ee.',
   },
 ];
 
